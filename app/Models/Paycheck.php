669858cf08d985f7ee97aab14067d83624e95e0b
daverydan <danny.w.avery@gmail.com>
2024-06-29 12:33:17 -0400

    ^app/Models/Paycheck.php

--- app/Models/Paycheck.php
+++ app/Models/Paycheck.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,23 +10,15 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Paycheck extends Model
 {
     use HasFactory;
+    use HasUuids;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
+        'uuid',
         'employee_id',
         'net_amount',
         'payed_at',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'id' => 'integer',
         'employee_id' => 'integer',
