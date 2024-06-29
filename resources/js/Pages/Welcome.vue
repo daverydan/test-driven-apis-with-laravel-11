b614cdb9724bbeaf9e6e8a2528a1d2efec7908fe
daverydan <danny.w.avery@gmail.com>
2024-06-28 23:12:41 -0400

    rm laravel & php versions from dashboard

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -168,7 +168,7 @@ function handleImageError() {
                 </main>
 
                 <footer class="py-16 text-center text-sm text-black dark:text-white/70">
-                    Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+                    <!-- Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }}) -->
                 </footer>
             </div>
         </div>
